18f1c8e40cff96058dfec71229626b1b73049462
luoxiaojun <luoxiaojun@dodoca.com>
2018-08-23 13:22:52 +0800

    style: formatting

--- src/foundation/util/string.go
+++ src/foundation/util/string.go
@@ -4,21 +4,23 @@ import (
 	"strings"
 )
 
+const LF = "\r\n"
+
 func Serialize(mapData map[string][]string) string {
 	var lines []string
 	for key, values := range mapData {
 		for _, value := range values {
-			lines = append(lines, key+"\r\n"+value)
+			lines = append(lines, key+LF+value)
 		}
 	}
-	return strings.Join(lines, "\r\n\r\n")
+	return strings.Join(lines, strings.Repeat(LF, 2))
 }
 
 func DeSerialize(data string) map[string]string {
 	mapData := make(map[string]string)
-	lines := strings.Split(data, "\r\n\r\n")
+	lines := strings.Split(data, strings.Repeat(LF, 2))
 	for _, line := range lines {
-		pair := strings.Split(line, "\r\n")
+		pair := strings.Split(line, LF)
 		mapData[pair[0]] = pair[1]
 	}
 	return mapData
