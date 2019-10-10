6269f91a523f0dab7a845a27c8b6a8565a919a30
luoxiaojun <luoxiaojun@dodoca.com>
2019-10-10 15:23:21 +0800

    fix: serializer bugfix

--- src/foundation/util/serializer.go
+++ src/foundation/util/serializer.go
@@ -5,12 +5,13 @@ import (
 )
 
 const CRLF = "\r\n"
+const ESCAPED_CRLF  = "\\r\\n"
 
 func Serialize(mapData map[string][]string) string {
 	var lines []string
 	for key, values := range mapData {
 		for _, value := range values {
-			lines = append(lines, key+CRLF+value)
+			lines = append(lines, EscapeCRLF(key)+CRLF+EscapeCRLF(value))
 		}
 	}
 	return strings.Join(lines, strings.Repeat(CRLF, 2))
@@ -21,7 +22,15 @@ func DeSerialize(data string) map[string]string {
 	lines := strings.Split(data, strings.Repeat(CRLF, 2))
 	for _, line := range lines {
 		pair := strings.Split(line, CRLF)
-		mapData[pair[0]] = pair[1]
+		mapData[UnEscapeCRLF(pair[0])] = UnEscapeCRLF(pair[1])
 	}
 	return mapData
+}
+
+func EscapeCRLF(str string) string {
+	return strings.Replace(str, CRLF, ESCAPED_CRLF, -1)
+}
+
+func UnEscapeCRLF(escapedStr string) string {
+	return strings.Replace(escapedStr, ESCAPED_CRLF, CRLF, -1)
 }
