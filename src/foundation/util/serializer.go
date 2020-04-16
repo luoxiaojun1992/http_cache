e8208adf05df6d41c9a32c056061e82422e147f8
luoxiaojun <luoxiaojun@dodoca.com>
2020-04-16 17:24:17 +0800

    fix: serializer bugfix

--- src/foundation/util/serializer.go
+++ src/foundation/util/serializer.go
@@ -4,33 +4,33 @@ import (
 	"strings"
 )
 
-const CRLF = "\r\n"
-const ESCAPED_CRLF  = "\\r\\n"
+const CR = "\r"
+const LF = "\n"
 
 func Serialize(mapData map[string][]string) string {
 	var lines []string
 	for key, values := range mapData {
 		for _, value := range values {
-			lines = append(lines, EscapeCRLF(key)+CRLF+EscapeCRLF(value))
+			lines = append(lines, EscapeCRLF(key)+CR+LF+EscapeCRLF(value))
 		}
 	}
-	return strings.Join(lines, strings.Repeat(CRLF, 2))
+	return strings.Join(lines, strings.Repeat(CR+LF, 2))
 }
 
 func DeSerialize(data string) map[string]string {
 	mapData := make(map[string]string)
-	lines := strings.Split(data, strings.Repeat(CRLF, 2))
+	lines := strings.Split(data, strings.Repeat(CR+LF, 2))
 	for _, line := range lines {
-		pair := strings.Split(line, CRLF)
+		pair := strings.Split(line, CR+LF)
 		mapData[UnEscapeCRLF(pair[0])] = UnEscapeCRLF(pair[1])
 	}
 	return mapData
 }
 
 func EscapeCRLF(str string) string {
-	return strings.Replace(str, CRLF, ESCAPED_CRLF, -1)
+	return strings.ReplaceAll(strings.ReplaceAll(str, "\r", "\\r"), "\n", "\\n")
 }
 
 func UnEscapeCRLF(escapedStr string) string {
-	return strings.Replace(escapedStr, ESCAPED_CRLF, CRLF, -1)
+	return strings.ReplaceAll(strings.ReplaceAll(escapedStr, "\\r", "\r"), "\\n", "\n")
 }
